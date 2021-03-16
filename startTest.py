4b8b1afff0645eda40deaf7c6811e696977f3201
Vladislav <59665801+ArsenalNox@users.noreply.github.com>
2021-03-15 21:16:57 -0700

    Add files via upload

--- startTest.py
+++ startTest.py
@@ -3,33 +3,20 @@ import mysql.connector
 import sys
 
 help = """
-    -s [args] - старт
 
-    [arguments: Названия проектов, для которых нужно начать тест через пробел]
+    [arguments: Названия бд, в которых нужно начать тест через пробел]
 """
 
 if len(sys.argv) > 1:
-    mydbUpper = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='',
-            database='sdosurp'
-            )
-    myUpperCursor = mydbUpper.cursor()
     for i in range(1, len(sys.argv)): #Проход по аргументам
-        sql = "SELECT * FROM main WHERE dir = '{}'".format(sys.argv[i])
-        print('Doing work... {}'.format((sys.argv[i])))
-        myUpperCursor.execute(sql)
-        result = myUpperCursor.fetchone()
+        print('Начинаем тест в {}...'.format((sys.argv[i])))
 
-        print("SQL: '{}'\nResult: {}".format(sql, result[1]))
         subDb = mysql.connector.connect(
                 host='localhost',
                 user='root',
                 password='',
-                database=result[1]
+                database=sys.argv[i]
                 )
-        #Занесение теста 
         subCursor = subDb.cursor()
         for i in range(1,5):
             sql = '''
@@ -43,11 +30,13 @@ if len(sys.argv) > 1:
                 )
             VALUES(
                 '2021-4-16',
-                `Контрольная работа`,
-                `Группа {}`,
-                `45`,
-                `Математика`,
-                `11`
+                'Контрольная работа',
+                'Группа {}',
+                '45',
+                'Математика',
+                '11'
             )
             '''.format(i)
             print('Выполнение запроса: "{}"'.format(sql))
+            subCursor.execute(sql)
+        subDb.commit()
